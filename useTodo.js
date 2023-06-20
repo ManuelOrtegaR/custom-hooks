2341b529d299a0573ebcc9e7861715101aff4ef1
Manuel Ortega Ruiz <manuellortegaruiz@gmail.com>
2023-06-19 21:13:55 -0500

    Primer commit

---
+++ useTodo.js
@@ -0,0 +1,53 @@
+import { useEffect, useReducer } from 'react';
+import { todoReducer } from '../08-useReducer/todoReducer';
+
+const initialState = [];
+
+const init = () => {
+  return JSON.parse(localStorage.getItem('todos')) || [];
+};
+
+export const useTodo = () => {
+  const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const handleNewTodo = (todo) => {
+    console.log(todo);
+    const action = {
+      type: '[TODO] Add Todo',
+      payload: todo,
+    };
+
+    dispatch(action);
+  };
+
+  const handleDeleteTodo = (id) => {
+    dispatch({
+      type: '[TODO] Remove Todo',
+      payload: id,
+    });
+  };
+
+  const onToggleTodo = (id) => {
+    dispatch({
+      type: '[TODO] Toggle Todo',
+      payload: id,
+    });
+  };
+
+  const todosCount = todos.length;
+
+  const pendingTodosCount = todos.filter((todo) => !todo.done).length;
+
+  return {
+    todos,
+    handleDeleteTodo,
+    handleNewTodo,
+    onToggleTodo,
+    todosCount,
+    pendingTodosCount,
+  };
+};
